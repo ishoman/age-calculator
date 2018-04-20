a88b5b5c29c3d5969486b04f59f78c842f89a32b
Isaac Shoman <isaac.shoman@gmail.com>
2018-04-20 10:23:32 -0700

    Add beforeEach, fail test for lack of object to import

--- spec/age-calculator-spec.js
+++ spec/age-calculator-spec.js
@@ -1,10 +1,16 @@
 import { Date } from './../src/age-calculator.js';
 
 describe("Date", function() {
-  const date = (2018, 3, 20)
-  let birthDate = new Date(2017, 3, 20)
+  const date = (2018, 3, 20);
+  let birthDate = new Date(2017, 3, 20);
+
+  beforeEach(function(){
+    let today = date.getTime();
+    let testDate = birthDate.getTime();
+  });
+
   it("should convert a given date into age in seconds", function(){
-    expect((date.getTime() - birthDate.getTime())*1000).toEqual(31571837.531)
+    expect((today - testDate) / 1000).toEqual(29894400);
   });
 
 });
