da6a95c3d0def85218eb6f1c7d8450e712d60293
Isaac Shoman <isaac.shoman@gmail.com>
2018-04-20 13:55:01 -0700

    refactor test code for spec 1

--- spec/age-calculator-spec.js
+++ spec/age-calculator-spec.js
@@ -1,13 +1,12 @@
 import { Planets } from './../src/age-calculator.js';
 
 describe("Date", function() {
-    let date = new Date(2018, 3, 20);
-    let birthDate = new Date(2017, 3, 20);
-    let today = date.getTime();
-    let testDate = birthDate.getTime();
 
   it("should convert a given date into age in seconds", function(){
-    expect((today - testDate) / 1000).toEqual(31536000);
+    debugger;
+    let date = new Date(2018, 3, 20);
+    let birthDate = new Date(2017, 3, 20);
+    expect((date.getTime() - birthDate.getTime()) / 1000).toEqual(31536000);
   });
 
   it("should convert age in earth years to age in Mercury years", function(){
