25cd977869e024b7ef222bc4bbe2ac02dbbe86ec
Isaac Shoman <isaac.shoman@gmail.com>
2018-04-20 12:27:10 -0700

    Add code for spec two test

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,1 +1,18 @@
+const date = new Date(2018, 3, 20);
+const birthDate = new Date(2017, 3, 20);
 
+export class Planets {
+  constructor (planet) {
+    this.planet = planet;
+  }
+
+  mercuryAge() {
+    let earthAge = (date.getTime() - birthDate.getTime()) / 1000;
+    let mercury = earthAge / .24;
+    return mercury;
+  }
+}
+
+function earthAge() {
+  return (date.getTime() - birthDate.getTime()) / 1000;
+}
